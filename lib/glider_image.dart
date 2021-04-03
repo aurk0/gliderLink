bef6bd5db6f49d473d1691b91a9e125438b730f8
Ferdous Zaman <febtesham162125@bscse.uiu.ac.bd>
2021-04-03 12:18:02 +0600

    init

---
+++ lib/glider_image.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GliderList extends StatefulWidget {
+  @override
+  _GliderListState createState() => _GliderListState();
+}
+
+class _GliderListState extends State<GliderList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
